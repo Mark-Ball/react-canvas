b959c0a775f2ea44f87ed2ec51cdd26f7b53d0e0
Mark-Ball <markball111@gmail.com>
2020-01-07 19:02:26 +1100

    implemented ability to change the title of drawing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Canvas from './Canvas';
 
 class App extends Component {
+    state = {
+        drawingName: ''
+    }
+
+    onChangeDrawingName = (event) => {
+        this.setState({ drawingName: event.target.value });
+    }
 
     render() {
+        const { drawingName } = this.state;
+
         return (
             <div>
-                <h1>Welcome to the drawing pad</h1>
+                <h1>{drawingName === '' ? 'Welcome to the drawing pad' : drawingName}</h1>
                 <Canvas />
+                <label>Drawing title</label>
+                <input onChange={this.onChangeDrawingName} type='text' />
             </div>
         )
     }
